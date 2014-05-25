70726578ad6c814ec240821c882f607e8544ea72
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2014-05-25 16:17:38 -0400

    Fixed LinkID view update issue

--- src/com/thslmit/watz/UIController/LinkIDController.java
+++ src/com/thslmit/watz/UIController/LinkIDController.java
@@ -3,18 +3,20 @@ package com.thslmit.watz.UIController;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.thslmit.watz.AppVar;
+import com.thslmit.watz.Main;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class LinkIDController implements Initializable {
 	
-	@FXML static Label deviceID;
-	
+	@FXML Label deviceID;
+
+	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-	}
-	
-	public void loadOnStart() {
-		System.out.println("LOAD");
+		String devID = AppVar.getDSDevID().substring(0, 8);
+		deviceID.setText(devID);
 	}
 }
